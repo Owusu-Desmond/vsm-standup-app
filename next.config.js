430c17144d5ce1045eb40ce7a5d567007aa361c6
Owusu-Desmond <desmond0406114009@gmail.com>
2025-09-20 10:36:12 +0000

    Fix page not found

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["*.preview.same-app.com"],
+  output: "export",
   images: {
     unoptimized: true,
     domains: [
